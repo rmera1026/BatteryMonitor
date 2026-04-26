c57ef4dc56e4853b3aa1c8e00cfa8eec6defe1e1
Ronald Mera <ronaldmera@mac.mynetworksettings.com>
2026-04-26 11:52:06 -0400

    Add modular architecture, UART logging, low battery alarm

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -50,7 +50,7 @@ ADC_HandleTypeDef hadc1;
 
 I2C_HandleTypeDef hi2c1;
 
-UART_HandleTypeDef huart1;
+UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
 uint8_t show_measurements = 0;
@@ -60,8 +60,8 @@ uint8_t show_measurements = 0;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
-static void MX_USART1_UART_Init(void);
 static void MX_ADC1_Init(void);
+static void MX_USART2_UART_Init(void);
 /* USER CODE BEGIN PFP */
 /* USER CODE END PFP */
 
@@ -98,8 +98,8 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_I2C1_Init();
-  MX_USART1_UART_Init();
   MX_ADC1_Init();
+  MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
 
   ssd1306_Init();
@@ -151,8 +151,8 @@ int main(void)
     /* UART log: "timestamp_ms,V.VV\r\n" */
     char log_buf[32];
     int log_len = snprintf(log_buf, sizeof(log_buf), "%lu,%lu.%02lu\r\n",
-        HAL_GetTick(), vin_cV / 100, vin_cV % 100);
-    HAL_UART_Transmit(&huart1, (uint8_t *)log_buf, (uint16_t)log_len, 100);
+    HAL_GetTick(), vin_cV / 100, vin_cV % 100);
+    HAL_UART_Transmit(&huart2, (uint8_t *)log_buf, (uint16_t)log_len, 100);
 
   }
   /* USER CODE END 3 */
@@ -283,35 +283,35 @@ static void MX_I2C1_Init(void)
 }
 
 /**
-  * @brief USART1 Initialization Function
+  * @brief USART2 Initialization Function
   * @param None
   * @retval None
   */
-static void MX_USART1_UART_Init(void)
+static void MX_USART2_UART_Init(void)
 {
 
-  /* USER CODE BEGIN USART1_Init 0 */
+  /* USER CODE BEGIN USART2_Init 0 */
 
-  /* USER CODE END USART1_Init 0 */
+  /* USER CODE END USART2_Init 0 */
 
-  /* USER CODE BEGIN USART1_Init 1 */
+  /* USER CODE BEGIN USART2_Init 1 */
 
-  /* USER CODE END USART1_Init 1 */
-  huart1.Instance = USART1;
-  huart1.Init.BaudRate = 115200;
-  huart1.Init.WordLength = UART_WORDLENGTH_8B;
-  huart1.Init.StopBits = UART_STOPBITS_1;
-  huart1.Init.Parity = UART_PARITY_NONE;
-  huart1.Init.Mode = UART_MODE_TX_RX;
-  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart1) != HAL_OK)
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN USART1_Init 2 */
+  /* USER CODE BEGIN USART2_Init 2 */
 
-  /* USER CODE END USART1_Init 2 */
+  /* USER CODE END USART2_Init 2 */
 
 }
 
--- Core/Src/ui.c
+++ Core/Src/ui.c
@@ -7,20 +7,18 @@ void UI_ShowIntro(void)
 {
     ssd1306_Fill(Black);
     ssd1306_SetCursor(0, 0);
-    ssd1306_WriteString("Voltage/Current", Font_7x10, White);
-    ssd1306_SetCursor(0, 12);
-    ssd1306_WriteString("Monitor", Font_7x10, White);
+    ssd1306_WriteString("Voltage Monitor", Font_7x10, White);
     ssd1306_SetCursor(0, 32);
     ssd1306_WriteString("Press button to", Font_7x10, White);
     ssd1306_SetCursor(0, 44);
-    ssd1306_WriteString("show measurements.", Font_7x10, White);
+    ssd1306_WriteString("show voltage.", Font_7x10, White);
     ssd1306_UpdateScreen();
 }
 
 void UI_ShowVoltage(uint32_t vin_cV)
 {
     char buf[32];
-    snprintf(buf, sizeof(buf), "Vin: ~%lu.%02luV", vin_cV / 100, vin_cV % 100);
+    snprintf(buf, sizeof(buf), "Voltage: %lu.%02luV", vin_cV / 100, vin_cV % 100);
     ssd1306_Fill(Black);
     ssd1306_SetCursor(0, 0);
     ssd1306_WriteString(buf, Font_7x10, White);
